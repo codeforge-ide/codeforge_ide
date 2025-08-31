862dc65e13aa5cf0ac822529fb4842a65452f153
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-08-31 10:22:27 +0100

    Revamp global styles and add Theme/Command providers
    Signed-off-by: nathfavour <116535483+nathfavour@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { IDELayout } from './components/layout/IDELayout.tsx';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "./themes";
+import { CommandProvider } from "./components/commands";
+import { IDELayout } from "./components/layout/IDELayout.tsx";
+import "./App.css";
 
 function App() {
-  return <IDELayout />;
+  return (
+    <ThemeProvider>
+      <CommandProvider>
+        <IDELayout />
+      </CommandProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
